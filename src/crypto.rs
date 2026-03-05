fe918932b90575adb58d25aac81ec73d98895bad
narukihto <Issaclex@proton.me>
2026-03-05 04:39:36 +0000

    Update crypto.rs

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,7 @@
 use pqcrypto_dilithium::dilithium2;
+// IMPORTANT: You must import these traits to use from_bytes()
+use pqcrypto_traits::sign::PublicKey as _; 
+use pqcrypto_traits::sign::DetachedSignature as _;
 
 pub struct QuantumCrypto;
 
@@ -8,7 +11,7 @@ impl QuantumCrypto {
         signature_bytes: &[u8],
         public_key_bytes: &[u8],
     ) -> bool {
-
+        // Now from_bytes will be found because the traits are in scope
         let pk = match dilithium2::PublicKey::from_bytes(public_key_bytes) {
             Ok(k) => k,
             Err(_) => return false,
